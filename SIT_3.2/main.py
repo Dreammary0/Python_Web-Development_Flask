5fbc54d9c90bc1dac662e9665ff55c36d972dfde
Dreammary0 <sazontova.md@students.dvfu.ru>
2022-11-03 01:52:39 +1000

    Отделила скрипт и стили

--- SIT_3.2/main.py
+++ SIT_3.2/main.py
@@ -1,7 +1,8 @@
 from jinja2 import Template
 import matplotlib.pyplot as plt
+import numpy as np
 
-def create_pict(x, y):
+def create_pict(x, y, way):
  #Построить линию графика, установить для нее цвет и толщину:
  line = plt.plot(x, y)
  plt.setp(line, color="blue", linewidth=2)
@@ -11,9 +12,10 @@ def create_pict(x, y):
  plt.gca().spines["top"].set_visible(False)
  plt.gca().spines["right"].set_visible(False)
  # Сохранть результат построения в файл:
- plt.savefig("pict.jpg")
+ plt.savefig(way)
+ a=way
  # Вернуть имя созданного файла
- return "pict.jpg"
+ return a
 
 def func(x):
     a=x*3 - 6*(x*x) + x + 5
@@ -32,7 +34,7 @@ def Zadanie1():
         x += h
 
     # Прочитать шаблон из файла function_template.html
-    f_template = open('function_template.html', 'r', encoding='utf-8-sig')
+    f_template = open('template_1/function_template.html', 'r', encoding='utf-8-sig')
     html = f_template.read()
     f_template.close()
 
@@ -41,10 +43,11 @@ def Zadanie1():
     # Указать, что в шаблоне будет использована функция len
     template.globals["len"] = len
     # Cоздадать файл для HTML-страницы
-    f = open('function.html', 'w', encoding='utf-8-sig')
+    f = open('template_1/function.html', 'w', encoding='utf-8-sig')
     # Сгенерировать страницу на основе шаблона
 
-    name_pict = create_pict(x_list, f_list)
+    way = "template_1/pict.jpg"
+    name_pict = create_pict(x_list, f_list, way)
 
     result_html = template.render(x=x_list, y=f_list, len=len, pict=name_pict)
     # Вывести сгенерированную страницу в файл
@@ -68,18 +71,22 @@ def Zadanie2():
     f_list = list()
     a = -2
     b = 6
-    n = 173
+    n = 30
     h =((b-a)/(n-1))
     x = a
-    while (x-h) < b:
-        x_list.append(x)
-        f_list.append(f_x(x,n_var))
-        x += h
+
+    for i in np.arange(x, b+h, h) :
+        print(x-h),
+        print(x),
+        print(h),
+        x_list.append(i),
+        f_list.append(f_x(i,n_var))
+
 
     count_f = len(list_name_f)
 
     # Прочитать шаблон из файла function_template.html
-    f_template = open('functions_template2.html', 'r', encoding='utf-8-sig')
+    f_template = open('template_2/functions_template2.html', 'r', encoding='utf-8-sig')
     html = f_template.read()
     f_template.close()
 
@@ -89,10 +96,12 @@ def Zadanie2():
     template.globals["len"] = len
     template.globals["round"] = round
     # Cоздадать файл для HTML-страницы
-    f = open('function2.html', 'w', encoding='utf-8-sig')
+    f = open('template_2/function2.html', 'w', encoding='utf-8-sig')
     # Сгенерировать страницу на основе шаблона
 
-    name_pict = create_pict(x_list, f_list)
+    way="template_2/pict.jpg"
+    name_pict = create_pict(x_list, f_list,way)
+
     result_html = template.render(list_f=list_name_f, count_f= count_f, x=x_list,
                                   y=f_list, pict=name_pict,a=a,b=b,n=n,
                                   name_f=list_name_f_long, n_var=n_var)
