076bc38d750f8eea24722349bee711a25d7dcdf9
Dreammary0 <sazontova.md@students.dvfu.ru>
2022-12-01 19:19:58 +1000

    исправленны

--- SIT_6.1/app.py
+++ SIT_6.1/app.py
@@ -3,7 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 import controllers.index
-import controllers.hello
 import controllers.subject
 import controllers.olympiad
 
--- SIT_6.1/controllers/index.py
+++ SIT_6.1/controllers/index.py
@@ -1,16 +1,59 @@
-from app import app
-from flask import render_template
 import constants
+import numpy as np
+from app import app
+from flask import render_template, request
 
 
 @app.route('/', methods=['GET'])
 def index():
-    # выводим форму
+    # задаем реквесты
+    name = request.values.get('username')
+    gender = request.values.get('gender')
+    program_id = request.values.get('program')
+    subject_id = request.values.getlist('subject[]')
+    olympiad_id = request.values.getlist('olympiad[]')
+
+    # формируем список из выбранных пользователем дисциплин и олимпиад
+    subjects_select = [constants.subjects[int(i)] for i in subject_id]
+    olympiads_select = [constants.olympiads[int(i)] for i in olympiad_id]
+
+    if not request.values.getlist('olympiad[]'): test=1
+    else: test=0
+
+    # Условия
+    if not name:
+        name = ''
+    if program_id:
+        program = constants.programs[int(program_id)]
+    else:
+        program = constants.programs[0]
+
+    #Рендерим главную страницу
     html = render_template(
         'index.html',
         program_list=constants.programs,
         subject_list=constants.subjects,
-        olympiads_list = constants.olympiads,
-        len=len
+        olympiad_list=constants.olympiads,
+        len=len,
+        name=name,
+        gender=gender,
+        program=program,
+        subjects_select=subjects_select,
+        olympiads_select=olympiads_select
+    )
+
+    #Добавляем вывод формы
+    html += render_template(
+        'hello.html',
+        program_list=constants.programs,
+        subject_list=constants.subjects,
+        olympiad_list=constants.olympiads,
+        len=len,
+        name=name,
+        gender=gender,
+        program=program,
+        subjects_select=subjects_select,
+        olympiads_select=olympiads_select,
+        test=test
     )
     return html
