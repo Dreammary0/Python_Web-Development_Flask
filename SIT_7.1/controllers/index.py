9d4b046c58a74b36d300256400a26006f545c9ab
Dreammary0 <sazontova.md@students.dvfu.ru>
2022-12-15 00:19:09 +1000

    Задание 7

--- SIT_7.1/controllers/index.py
+++ SIT_7.1/controllers/index.py
@@ -4,7 +4,6 @@ import sqlite3
 from utils import get_db_connection
 from models.index_model import get_reader, get_book_reader, borrow_book, get_new_reader
 #from models.book import return_book
-
 @app.route('/', methods=['get'])
 def index():
     conn = get_db_connection()
@@ -12,30 +11,34 @@ def index():
     if request.values.get('reader'):
         reader_id = int(request.values.get('reader'))
         session['reader_id'] = reader_id
-     # нажата кнопка Добавить со страницы Новый читатель# (взять в комментарии, пока не реализована страница Новый читатель)
-    elif request.values.get('new_reader'):
-        new_reader = request.values.get('new_reader')
-        session['reader_id'] = get_new_reader(conn, new_reader)
-        # нажата кнопка Взять со страницы Поиск
-        # (взять в комментарии, пока не реализована страница Поиск)
+    # нажата кнопка Добавить со страницы Новый читатель
+    # (взять в комментарии, пока не реализована страница Новый читатель)
+    elif request.values.get('submitNewReader'):
+        html=render_template('new_reader.html')
+        return html
+    elif request.values.get('newReaderFio'):
+        r_id=get_new_reader(conn, request.values.get('newReaderFio'))
+        print(r_id)
+        session['reader_id'] = int(r_id)
+    elif request.values.get('return'):
+        reader_book_id = int(request.values.get('return'))
+        print(reader_book_id)
+        return_book(conn, reader_book_id)
     elif request.values.get('book'):
         book_id = int(request.values.get('book'))
         borrow_book(conn, book_id, session['reader_id'])
-        # нажата кнопка Не брать книгу со страницы Поиск
     elif request.values.get('noselect'):
         a = 1
-        # вошли первый раз
     else:
-        session['reader_id'] = 1
-        df_reader = get_reader(conn)
-        df_book_reader = get_book_reader(conn, session['reader_id'])
+        session['reader_id']= 3
+    df_reader = get_reader(conn)
+    df_book_reader = get_book_reader(conn, session['reader_id'])
 
-        # выводим форму
-        html = render_template(
-            'index.html',
-            reader_id=session['reader_id'],
-            combo_box=df_reader,
-            book_reader=df_book_reader,
-            len=len
-        )
+    html = render_template(
+        'index.html',
+        reader_id = session['reader_id'],
+        combo_box = df_reader,
+        book_reader = df_book_reader,
+        len = len
+    )
     return html
---
+++ SIT_7_flask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, session
+
+app = Flask(__name__)
+# установим секретный ключ для подписи.
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+import controllers.index
+import controllers.search
+
+if __name__ == '__main__':
+    app.run()
